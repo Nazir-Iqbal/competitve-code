c35dcd3e2f515383893c0894d8d784dcaf8439a1
Nazir-Iqbal <naziriqbal01102003@gmail.com>
2025-05-19 17:31:24 +0000

    day0

--- Problemset/test.cpp
+++ Problemset/test.cpp
@@ -12,26 +12,10 @@ using namespace std;
 // priority_queue <int, vector<int>, greater<int>> pq;
 // template <class T> using oset = tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
 
-bool check(int arr[],int n,int w,int mid){
-    int time=0;
-    for(int i=0;i<n;i++){
-        time += (arr[i]+mid-1)/mid;
-    }
-    if(time>w) return false;
-    return true;
-}
-
 void solve(){
-    int n,w;cin>>n>>w;
-    int arr[n];
-    for(int i=0;i<n;i++) cin>>arr[i];
-    long long lo=1,hi = 1e9;
-    while(lo<=hi){
-        int mid = (lo+hi)/2;
-        if(check(arr,n,w,mid)) hi = mid-1;
-        else lo = mid+1;
-    }
-    cout<<lo<<endl;
+    int n;cin>>n;
+    cout<<n<<endl;
+    cout<<"This is the testing code";
 }
 
 int32_t main(){
